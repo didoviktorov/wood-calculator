eb7f70dbce25a0f7b252ce78412db25f5ea4674c
Deyan Chakarov <dchakarov@shopmetrics.com>
2020-03-26 17:19:08 +0200

    pure width upper  cabinets fix

--- src/components/UpperCabinets.vue
+++ src/components/UpperCabinets.vue
@@ -587,7 +587,7 @@ export default {
           remainingWidth += this.$store.state.upperShelf.cabinets[cabinetIndex]
             .outerWidth;
         }
-        remainingWidth -= parseInt(this.cabinets[cabinetIndex].outerWidth);
+        remainingWidth -= this.cabinets[cabinetIndex].outerWidth;
       }
 
       return remainingWidth;
